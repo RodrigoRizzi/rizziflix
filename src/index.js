f8d8775cff2421e54341a99158398634d70b0d46
Rodrigo A. Rizzi <roarizzi2@hotmail.com>
2020-08-04 21:14:06 -0300

    atualização para funcionar front e back pelo servidor (package.json)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; // import para a implementação do SPA
 import Home from './pages/home';
 import CadastroVideo from './pages/cadastro/video';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'; //import para a implementação do SPA 
-import CadastroCategoria from './pages/cadastro/categoria/indes';
-//--- SWITCH serve como IF (ou switch case) para esolher a rota (URL) da aplicação 
-//--- ROUTE mostra o que renderizará, a página...
+import CadastroCategoria from './pages/cadastro/categoria';
+// --- SWITCH serve como IF (ou switch case) para esolher a rota (URL) da aplicação
+// --- ROUTE mostra o que renderizará, a página...
 
 ReactDOM.render(
-  
+
   <BrowserRouter>
-    <Switch> 
-      <Route path="/" component={Home} exact></Route>
-      <Route path="/cadastro/video" component={CadastroVideo} exact></Route>
-      <Route path="/cadastro/categoria" component={CadastroCategoria} exact></Route>
-      <Route component={() => (<div>Página 404</div>)}></Route>
-    </Switch>    
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route component={() => (<div>Página 404</div>)} />
+    </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/pageDefault';
 import FormField from '../../../components/formField';
@@ -28,6 +28,23 @@ function CadastroCategoria() {
       infosDoEvento.target.value,
     );
   }
+
+  // useEffect() realiza uma ação (efeito) com base em outra ação. Like ajax
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategorias(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
 
   return (
     <PageDefault>
